7447d4d317f3b44f9ba30283bdd8705aca416ec7
sergo0109 <79332413+sergo0109@users.noreply.github.com>
2021-04-24 21:14:43 +0400

    update

--- src/users/usersDto/create-user.dto.ts
+++ src/users/usersDto/create-user.dto.ts
@@ -1,24 +1,21 @@
-import {IsDateString, IsEmail, IsPhoneNumber, IsString, Length} from "class-validator";
+import { IsDateString, IsEmail, IsPhoneNumber, Length } from 'class-validator';
 
+export class CreateUserDto {
+  @Length(3, 30, { message: 'name must be 3-30' })
+  readonly name: string;
 
-export class CreateUserDto{
+  @Length(3, 15, { message: 'userName must be 5-15' })
+  readonly userName: string;
 
-    @Length(3,30, {message:"name must be 3-30"})
-    readonly name:string
+  @IsDateString()
+  readonly DOB: string;
 
-    @Length(3,15, {message:"userName must be 5-15"})
-    readonly userName:string
+  @IsEmail()
+  readonly email: string;
 
-    @IsDateString()
-    readonly DOB:string
+  @IsPhoneNumber('AM', { message: 'phone must be  phone number' })
+  readonly phone: number;
 
-    @IsEmail()
-    readonly email:string
-
-    @IsPhoneNumber("AM",{message:"phone must be  phone number"})
-    readonly phone:number
-
-    @Length(8,20,{message:"password must be 5-15"})
-    password:string
-
+  @Length(8, 20, { message: 'password must be 5-15' })
+  password: string;
 }
